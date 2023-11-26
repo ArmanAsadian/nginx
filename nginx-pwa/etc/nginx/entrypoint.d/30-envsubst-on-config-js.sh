4f2b487b57086a18b3c6951ca9e7399baf65599d
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-11-26 14:35:41 +0330

    feat(nginx-pwa): enhance 30-envsubst-on-config-js.sh

--- nginx-pwa/etc/nginx/entrypoint.d/30-envsubst-on-config-js.sh
+++ nginx-pwa/etc/nginx/entrypoint.d/30-envsubst-on-config-js.sh
@@ -5,20 +5,23 @@ set -e
 
 ME=$(basename $0)
 
-config_js_envsubst() {
-  local config_js_path="${NGINX_ENVSUBST_CONFIG_JS_PATH:-/var/www/html/config.js}"
-
-  local defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-  [ -f "$config_js_path" ] || return 0
-  if [ ! -w "$config_js_path" ]; then
-    echo >&3 "$ME: ERROR: $config_js_path exists, but is not writable"
+configJsEnvsubst() {
+  local configJsPath="$NGINX_CONFIG_JS_PATH"
+  if [ -z "$configJsPath" ] || [ ! -f "$configJsPath" ]
+  then
     return 0
   fi
-  echo >&3 "$ME: Running envsubst on $config_js_path"
-  envsubst "$defined_envs" < "$config_js_path" > "${config_js_path}.tmp"
-  mv -f "${config_js_path}.tmp" "$config_js_path"
+
+  local definedEnvs=$(printf '${%s} ' $(env | cut -d= -f1))
+  if [ ! -w "$configJsPath" ]; then
+    echo >&3 "$ME: ERROR: $configJsPath exists, but is not writable"
+    return 0
+  fi
+  echo >&3 "$ME: Running envsubst on $configJsPath"
+  envsubst "$definedEnvs" < "$configJsPath" > "${configJsPath}.tmp"
+  mv -f "${configJsPath}.tmp" "$configJsPath"
 }
 
-config_js_envsubst
+configJsEnvsubst
 
 exit 0
