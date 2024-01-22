b233d272cc4c881925220291adaddf784c381489
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-22 16:23:23 +0330

    refactor(core/entrypoint): remove function and add logger

--- nginx-core/etc/nginx/entrypoint.d/30-copy-default-data.sh
+++ nginx-core/etc/nginx/entrypoint.d/30-copy-default-data.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -eu
 
+ME=$(basename "$0")
+
 if [ -d "/default-data" ]; then
   # copy without overwriting existing files
+  echo "$ME: Copy default data from /default-data to $NGINX_DOCUMENT_ROOT"
   cp -anv /default-data/* $NGINX_DOCUMENT_ROOT/
 fi
 
