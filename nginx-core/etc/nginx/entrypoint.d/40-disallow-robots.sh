8617bd4eac8d6a08b76edfa008d97e745319ba2f
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-01-23 15:50:40 +0330

    fix(nginx-core): disallow robots script
    Co-authored-by: S. Amir Mohammad Najafi <njfamirm@gmail.com>

--- nginx-core/etc/nginx/entrypoint.d/40-disallow-robots.sh
+++ nginx-core/etc/nginx/entrypoint.d/40-disallow-robots.sh
@@ -4,10 +4,9 @@ set -eu
 
 ME=$(basename "$0")
 
-# replace the default robots.txt with a disallow all
-if [ ${NGINX_DISALLOW_ROBOTS:-} == 1 ]; then
-  echo "$ME: Replace robots.txt to disallow all robots"
-  cp -afv /default-data/robots.txt $NGINX_DOCUMENT_ROOT/
-fi
+test -z "${NGINX_DISALLOW_ROBOTS:-}" && exit 0
+
+echo "$ME: Replace robots.txt to disallow all robots"
+cp -afv /default-data/robots.txt $NGINX_DOCUMENT_ROOT/
 
 exit 0
